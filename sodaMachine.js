8b90bbddda559743ee5cf4b28a285aab5188281c
Pauolo <Pauolosavery1@gmail.com>
2022-05-26 13:39:02 +0300

    All test r passed

--- sodaMachine.js
+++ sodaMachine.js
@@ -5,12 +5,21 @@ class SodaMachine {
   }
 
   currentInventoryCount() {
+    return this.sodas.length;
   }
 
   findSoda(sodaBrand) {
+    return this.sodas.find((obj) => obj.brand === sodaBrand);
   }
 
   sell(sodaBrand) {
+    const elem = this.sodas.find((obj) => obj.brand === sodaBrand);
+    if (elem) {
+      this.sodas.splice(this.sodas.indexOf(elem), 1);
+      this.cash += elem.price;
+      return elem;
+    }
+    return undefined;
   }
 }
 
